86b75ec3b8ae0d6a0ffd0349a26eab9d0959ef33
Gideon Lingle <gideon.lingle1@gmail.com>
2019-01-17 20:59:08 -0600

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -7,24 +7,20 @@ module.exports.run = async (bot, message, args) => {
   const msgmember = message.member;
   if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("I don't have permission to do that. Make sure I have the Ban Members permission.");
   if (!msgmember.hasPermission("BAN_MEMBERS")) return message.channel.send("Invalid permissions!");
-  
-  if (member.bannable) {
-    await member.ban(reason || null)
-          .then(member => {
-            const em = new discord.RichEmbed()
-            .addField("Modboi Ban System", `🔨 ${member.user.username} is outta here!`)
-            .setFooter(`${member.user.username} was banned by ${msgmember.displayName}.`)
-            .setTimestamp()
-            .setColor("GREEN");
-            message.channel.send({embed: em})
-          })
-  
-          .catch(err => {
-            message.channel.send(`I couldn't ban ${member.displayName}...`)    
-            console.error(`[ERROR] Ban Failed in guild ${member.guild.name}.\nError Message: ${err}`)
-          })
-  }
-  
+  if (!member.bannable) return message.channel.send(`${member.displayName} is not bannable.`);
+  await member.ban(reason || null)
+        .then(member => {
+          const em = new discord.RichEmbed()
+          .addField("Modboi Ban System", `🔨 ${member.user.username} is outta here!`)
+          .setFooter(`${member.user.username} was banned by ${msgmember.displayName}.`)
+          .setTimestamp()
+          .setColor("GREEN");
+          message.channel.send({embed: em})
+        })
+        .catch(err => {
+          message.channel.send(`I couldn't ban ${member.displayName}...`)    
+          console.error(`[ERROR] Ban Failed in guild ${member.guild.name}.\nError Message: ${err}`)
+        })  
 }
 
 module.exports.help = {name: "ban"}
