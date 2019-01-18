a2239d32913387b665fd14edfbb26741110d1107
Gideon Lingle <gideon.lingle1@gmail.com>
2019-01-17 22:27:27 -0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 const discord = require('discord.js')
 const categories = ["⚒ Moderation", "🍭 Fun", "🤖 Bot", "🎵 Music"]
-const commands = fs.readdirSync('./commands/')
+const commands = require('fs').readdirSync('./commands/')
 
 module.exports.run = (bot, message, args) => {
   console.log(require(`./${commands}`).help.name)
