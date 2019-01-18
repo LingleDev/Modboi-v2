0eded71c70b63bac9c9d942f76890b377dffef79
Gideon Lingle <gideon.lingle1@gmail.com>
2019-01-17 22:43:12 -0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,25 @@
 const discord = require('discord.js')
-const categories = ["⚒ Moderation", "🍭 Fun", "🤖 Bot", "🎵 Music"]
-const commands = require('fs').readdirSync('./commands/')
+const category_displaynames = ["⚒ Moderation", "🍭 Fun", "🤖 Bot", "🎵 Music"]
+const categories = ["Moderation", "Fun", "Bot", "Music"]
 
 module.exports.run = (bot, message, args) => {
-  console.log(require(`./${commands}`).help.name)
+  const msg = args.join(" ");
+  
+  for (const cmds in bot.commands) {
+    console.log(cmds)
+  }
+  
+  if (categories.includes(msg)) {
+    const em = new discord.RichEmbed()
+    .addField("Modboi v2 Help Menu", `Category: ${msg}\n`, true)
+  } else {
+    const cmd = bot.commands.get(msg)
+    
+    if (cmd) {
+      const em = new discord.RichEmbed()
+      .addField("Modboi v2 Help Menu", ``, true)
+    }
+  }
 }
 
 module.exports.help = {name: "help"}
