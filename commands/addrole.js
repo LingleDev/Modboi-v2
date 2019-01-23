b146f8e85d2bc74583166230ed87cc2850bce16f
Gideon Lingle <gideon.lingle1@gmail.com>
2019-01-23 09:58:37 -0600

    Create addrole.js

---
+++ commands/addrole.js
@@ -0,0 +1,11 @@
+module.exports.run = (bot, message, args) => {
+  const member = message.mentions.members.first();
+  const role = message.mentions.roles.first();
+  const name = role.name
+  
+  
+}
+
+module.exports.help = {
+  name: "addrole"
+}
