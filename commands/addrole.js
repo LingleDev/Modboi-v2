e5fbef4fd7e362675cdb68400c103aa18876b2a7
Gideon Lingle <gideon.lingle1@gmail.com>
2019-01-23 10:44:36 -0600

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -4,7 +4,7 @@ module.exports.run = (bot, message, args) => {
   const name = role.name
   
   if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send(`Invalid permissions!`);
-  if (!message.guild.me.hasPermission()) return message.channel.send(`I can't do that. Make sure I have the Manage Roles permission.`);
+  if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send(`I can't do that. Make sure I have the Manage Roles permission.`);
   
   member.addRole(role)
   .then(m => {
