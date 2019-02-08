d384b2d09eb32e1c8fa08dc14b40a6d59b12b451
Gideon Lingle <gideon.lingle1@gmail.com>
2019-02-07 18:20:18 -0600

    Update role_info.js

--- commands/role_info.js
+++ commands/role_info.js
@@ -6,5 +6,24 @@ module.exports.run = (bot, message, args) => {
   if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("Invalid permissions!");
   if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send("I can't do that... Make sure I have the Manage Roles permission.");
   
+  const n = role.name
+  const i = role.id
+  const hoisted = role.hoist
+  const canBeEdited = role.editable
+  const mentioned = role.mentionable
+  const hex = role.hexColor
+  
+  
   const em = new (require('discord.js').RichEmbed)()
+  .addField("Modboi v2 Role Info:", "Here's the requested info!",true)
+  .addField("Role Name:", n,true)
+  .addField("Role ID:", i,true)
+  .addField("Role Hoist:", hoisted,true)
+  .addField("Role Editable:", canBeEdited,true)
+  .addField("Role Mentionable:", mentioned, true)
+  .addField("Role Color Hex:", hex, true)
+  .setTimestamp()
+  .setFooter("Requested by %s".replace("%s", message.member.displayName))
+  .setColor("GREEN")
+  message.channel.send({embed: em})
 }
