fdfed4fc98c8aa63895780b7df9339539bd5e996
Gideon Lingle <gideon.lingle1@gmail.com>
2019-02-08 12:50:14 -0600

    Update role_ids.js

--- commands/role_ids.js
+++ commands/role_ids.js
@@ -2,15 +2,18 @@ module.exports.help = {name: "roles"}
 const api = new (require('pastebin-js'))(process.env.pb_key)
 module.exports.run = (bot, message, args) => {
   const roles = message.guild.roles
+  var link;
   
   roles.forEach((i,r) => {
     api.createPaste(`Role ID: ${i} Role Name: ${r.name}\n`, `Roles for ${message.guild.name}` ).then(d => {
-      const em = new (require('discord.js').RichEmbed)()
-      .addField("Modboi v2 Role Mapper", "I got a list of the roles in this guild.")
-      .addField("Pastebin Link", `I posted the roles to Pastebin, so it wouldn't take up space in this chat. You can find the paste [here](${d} "Check out this paste.").`)
-      .setTimestamp()
-      .setColor("GREEN")
-      message.channel.send({embed: em})
+      link = d
     })
   })
+  
+  const em = new (require('discord.js').RichEmbed)()
+  .addField("Modboi v2 Role Mapper", "I got a list of the roles in this guild.")
+  .addField("Pastebin Link", `I posted the roles to Pastebin, so it wouldn't take up space in this chat. You can find the paste [here](${d} "Check out this paste.").`)
+  .setTimestamp()
+  .setColor("GREEN")
+  message.channel.send({embed: em})
 }
