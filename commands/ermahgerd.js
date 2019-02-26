a4bc41b6e21dce10ad42724bb201a5968bf02ed6
Gideon Lingle <gideon.lingle1@gmail.com>
2019-02-26 12:17:10 -0600

    Update ermahgerd.js

--- commands/ermahgerd.js
+++ commands/ermahgerd.js
@@ -1,3 +1,15 @@
+const ermahgerd = require('node-ermahgerd')
+
 module.exports.help = {name: "ermahgerd"}
 
-module.exports.run = () => {}
+module.exports.run = (bot, message, args) => {
+  const msg = args.join(" ");
+  const translation = ermahgerd.translate(msg)
+  const em = new (require('discord.js').RichEmbed)()
+  .setTitle("Modboi v2 ERMAHGERD")
+  .setDescription(`I translated your message!`)
+  .addField("Translation:", translation, false)
+  .setTimestamp()
+  .setFooter("ERMAHGERD")
+  message.channel.send({embed: em})
+}
