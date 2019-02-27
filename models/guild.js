e8151cb1e79bbc7e4d6fd95ef23e8e0f707a31e0
Gideon Lingle <gideon.lingle1@gmail.com>
2019-02-27 10:37:23 -0600

    Added modlog and welcome
    You can now set the mod log/welcome channels

--- models/guild.js
+++ models/guild.js
@@ -4,7 +4,9 @@ const schema = mongoose.Schema({
   guildId: String,
   guildName: String,
   premium: { type: Boolean, default: false },
-  blacklist: { type: Boolean, default: false }
+  blacklist: { type: Boolean, default: false },
+  modlog: { type: String, default: 0 },
+  welcome: { type: String, default: 0 }
 })
 
 module.exports = mongoose.model("gConf", schema)
