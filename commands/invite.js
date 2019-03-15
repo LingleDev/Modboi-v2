8578777c7a2702729143644d14d6d81d8aa081ea
Gideon Lingle <gideon.lingle1@gmail.com>
2019-03-14 22:07:55 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,1 +1,11 @@
+module.exports.run = (bot, message, args) => {
+  const inv = bot.invite
+  
+  const em = new (require('discord.js').RichEmbed)()
+  .addField("Modboi v2 Invite", `Thanks for inviting me! [Here](${inv} "invite the bot")'s the invite.`, false)
+  .setTimestamp()
+  .setColor("GREEN");
+  message.channel.send({embed: em})
+}
+
 module.exports.help = {name: "invite"}
