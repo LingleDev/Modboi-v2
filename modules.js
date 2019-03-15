3054a6706169ad5956db0cdd5a002f1e6cebe918
Gideon Lingle <gideon.lingle1@gmail.com>
2019-03-14 22:24:13 -0500

    Update modules.js

--- modules.js
+++ modules.js
@@ -1,6 +1,7 @@
 module.exports = {
   PermModule: require('./util/permchecker.js'),
   UserSettings: require('./models/user.js'),
+  GuildSettings: require('./models/guild.js'),
   AutoPoster: require('./util/auto_poster.js'),
   Music: require('./music/MusicHandler.js')
 }
