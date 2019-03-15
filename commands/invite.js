2b56bfca823515c2cdf65923a1f62f87f28d7b4c
Gideon Lingle <gideon.lingle1@gmail.com>
2019-03-14 22:15:15 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ module.exports.run = (bot, message, args) => {
   
   const em = new (require('discord.js').RichEmbed)()
   .setTitle("Modboi v2 Invite")
-  .setDescription(`I can't use invite links! [Here](${inv})'s my invite link!`)
+  .setDescription(`I can't use invite links! [Here](${inv} "invite the bot")'s my invite link!`)
   .setTimestamp()
   .setColor("GREEN");
   message.channel.send({embed: em})
