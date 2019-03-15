c29271263aeff8908958d8cbee43072164958ddc
Gideon Lingle <gideon.lingle1@gmail.com>
2019-03-14 22:02:19 -0500

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,1 +1,10 @@
+const MusicHandler = require('../modules').Music
 
+
+module.exports.run = (bot, message, args) => {
+  let Music = new MusicHandler({ member: message.member })
+  
+  
+}
+
+module.exports.help = {name: "play"}
