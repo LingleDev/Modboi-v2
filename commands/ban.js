f6ab9355984dd2e9db2f0a0c05b49f1cbba825ac
Gideon Lingle <gideon.lingle1@gmail.com>
2019-03-15 00:59:11 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,12 +4,12 @@ const guild = require('../modules').GuildSettings
 module.exports.run = async (bot, message, args) => {
   const member = message.mentions.members.first();
   if (!member) return message.channel.send("Please mention a member to ban.");
-  const reason = args.slice(1).join(" ");
+  const reason = args.slice(1).join(" ") || "No reason provided.";
   const msgmember = message.member;
   if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("I don't have permission to do that. Make sure I have the Ban Members permission.");
   if (!msgmember.hasPermission("BAN_MEMBERS")) return message.channel.send("Invalid permissions!");
   if (!member.bannable) return message.channel.send(`${member.displayName} is not bannable.`);
-  await member.ban(reason || null)
+  await member.ban(reason)
         .then(member => {
           guild.findOne({ guildId: message.guild.id }, (err,data) => {
             if (!data) {
